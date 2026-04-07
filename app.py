4be2ae9f9be44558ea4d8948c036a7c3431ac49e
evgenpenkov777-sys <evgenpenkov777@gmail.com>
2026-04-07 16:24:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -209,7 +209,11 @@ if st.button("Найти минимальные расстояния"):
             dist, prev = dijkstra(graph, start_vertex)
 
             col1, col2 = st.columns([1.2, 1])
-
+with col1:
+    st.subheader("3) Визуализация графа")
+    dot = make_dot(edges, start_vertex)
+    st.graphviz_chart(dot, use_container_width=True)
+    
 with col2:
     st.subheader("4) Результат")
 
